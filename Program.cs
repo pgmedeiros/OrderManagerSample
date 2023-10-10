e5fc89f4f82b9e525425652b343a7ca0e6bfdbd5
medeiros <emaildopedromedeiros@gmail.com>
2023-10-10 19:41:25 -0300

    inicia projeto

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using QuickFix;
+using QuickFix.Fields;
+using teste;
+public class Program
+{
+
+    static void Main()
+    {
+
+        QuickFixApp quickFixApp = new QuickFixApp();
+        SessionSettings settings = new SessionSettings("C:/estudo/teste/config.txt");
+
+        IMessageStoreFactory storeFactory = new QuickFix.FileStoreFactory(settings);
+        ILogFactory logFactory = new QuickFix.ScreenLogFactory(settings);
+        QuickFix.Transport.SocketInitiator initiator = new QuickFix.Transport.SocketInitiator(quickFixApp, storeFactory, settings, logFactory);
+
+        // this is a developer-test kludge.  do not emulate.
+
+        initiator.Start();
+
+        quickFixApp.createOrder();
+
+        initiator.Stop();
+          
+    }
+}
---
+++ QuickFixApp.cs
@@ -0,0 +1,60 @@
+﻿using QuickFix;
+using QuickFix.Fields;
+
+namespace teste
+{
+    internal class QuickFixApp : IApplication
+    {
+
+        private SessionID MySessionID = null;
+        public void ToAdmin(Message message, SessionID sessionID)
+        {
+            
+        }
+
+        public void FromAdmin(Message message, SessionID sessionID)
+        {
+        }
+
+        public void ToApp(Message message, SessionID sessionID)
+        {
+            
+        }
+
+        public void FromApp(Message message, SessionID sessionID)
+        {
+        }
+
+        public void OnCreate(SessionID sessionID)
+        {
+            MySessionID = sessionID;
+        }
+
+        public SessionID getSessionId()
+        {
+            return this.MySessionID;
+        }
+
+        public void OnLogout(SessionID sessionID)
+        {
+        }
+
+        public void OnLogon(SessionID sessionID)
+        {
+            Console.WriteLine("session id" + sessionID.ToString);
+        }
+
+        public void createOrder()
+        {
+            var order = new QuickFix.FIX44.NewOrderSingle(
+            new ClOrdID("1234"),
+            new Symbol("AAPL"),
+            new Side(Side.BUY),
+            new TransactTime(DateTime.Now),
+            new OrdType(OrdType.MARKET));
+
+            Session.SendToTarget(order, MySessionID);
+        }
+
+    }
+}
